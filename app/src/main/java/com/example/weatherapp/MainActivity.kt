3b5f9036bb4a4f211ec596ffe6575eb32685c73a
Troldmand43 <Tobiaseottsen@gmail.com>
2022-04-25 16:12:34 +0200

    Xml File update

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.weatherapp
 
+
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//Imported for date
+import android.widget.TextView
+import java.text.SimpleDateFormat
+
+
 class MainActivity : AppCompatActivity() {
+
+    //Date variable
+    lateinit var textView: TextView
+    lateinit var date: String
+    lateinit var simpleDateFormat : SimpleDateFormat
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.date_ID)
+
     }
+
+
 }
