f010ae6b17159e5c735435eb477a44034d1af99b
Petr Češka <ceskape@seznam.cz>
2022-04-25 23:20:31 +0200

    after merge repairs

--- app/src/main/java/com/example/weatherapp/view/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/view/MainActivity.kt
@@ -7,6 +7,11 @@ import com.example.weatherapp.R
 import com.example.weatherapp.viewmodel.WeatherAppViewModel
 import androidx.activity.viewModels
 
+//Imported for date
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+
 class MainActivity : AppCompatActivity() {
 
     private val WeatherAppViewModel: WeatherAppViewModel by viewModels()
@@ -15,10 +20,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView: TextView = findViewById(R.id.date_ID)
+        val calendar: Calendar = Calendar.getInstance()
+        val simpleDateFormat = SimpleDateFormat("EEEE, dd MMMM ")
+        val dateTime = simpleDateFormat.format(calendar.time)
+        textView.text = dateTime
+
         WeatherAppViewModel.dayWeather.observe(
             this
         ) { test ->
             Log.i(null, test)
         }
     }
+
+
 }
