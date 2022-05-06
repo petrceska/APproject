2496c3ffb09e81b3acba992c0120527aceb771ce
Troldmand43 <Tobiaseottsen@gmail.com>
2022-05-07 01:48:08 +0200

    Small update on xml files

---
+++ app/src/main/java/com/example/weatherapp/view/Second_activity.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weatherapp.R
+
+class Second_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
