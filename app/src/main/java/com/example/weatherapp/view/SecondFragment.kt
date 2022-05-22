d5676017d778d5eba82c0838128c4fc8e8add7fa
Troldmand43 <Tobiaseottsen@gmail.com>
2022-05-22 18:20:05 +0200

    Update of recyclerview in second fragment

--- app/src/main/java/com/example/weatherapp/view/SecondFragment.kt
+++ app/src/main/java/com/example/weatherapp/view/SecondFragment.kt
@@ -1,15 +1,23 @@
 package com.example.weatherapp.view
 
-import androidx.fragment.app.Fragment
-import com.example.weatherapp.R
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import kotlinx.android.synthetic.main.activity_second.*
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
+import com.example.weatherapp.R
+import kotlinx.android.synthetic.main.activity_second.*
+import kotlinx.android.synthetic.main.city_view.*
+import kotlinx.android.synthetic.main.fragment_second.*
 
 
 class SecondFragment : Fragment (R.layout.fragment_second) {
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<cityAdapter.CityViewHolder>? = null
 
     //Data to populate the recyclerview
     var cityList = mutableListOf(
@@ -21,19 +29,41 @@ class SecondFragment : Fragment (R.layout.fragment_second) {
         City(29,"Odense","Denmark",4,4.4)
     )
 
-    GridLayoutManager(this, //Context 2, //Span count
-    RecyclerView.VERTICAL, //orientation
-    false //  reverse layout
-    ).apply {
-        // specify the layout manager for recycler view
-        Recycler_ID.layoutManager = this
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
-    val adapter = cityAdapter(cityList)
-    Recycler_ID.adapter = adapter
+
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(itemView, savedInstanceState)
+        recyclerview_id.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = LinearLayoutManager(activity)
+            // set the custom adapter to the RecyclerView
+            adapter = cityAdapter(cityList)
+
+        }
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
 
     //Recycler_ID.layoutManager = LinearLayoutManager(this)
 
     //Setup search function here
     //searchbar_id.
-}
