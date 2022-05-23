4c897f1c348679bfc04d3fbf5698745387b840d7
Petr Češka <ceskape@seznam.cz>
2022-05-23 17:52:11 +0200

    select forecast

--- app/src/main/java/com/example/weatherapp/database/ForecastDao.kt
+++ app/src/main/java/com/example/weatherapp/database/ForecastDao.kt
@@ -8,8 +8,8 @@ interface ForecastDao {
     @Query("SELECT * FROM forecast where id = :id")
     fun getForecastById(id : Int): Forecast?
 
-    @Query("SELECT * FROM forecast where cityName = :cityName")
-    fun getForecastByCityName(cityName : String): Array<Forecast>
+    @Query("SELECT * FROM forecast where cityName = :cityName order by date_time limit 3")
+    fun getForecastByCityName(cityName : String, dateTime: String): Array<Forecast>
 
     @Update
     fun update(forecast: Forecast): Int
