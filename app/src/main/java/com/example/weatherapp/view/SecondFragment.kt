7dc6a34b23bc7f877c32d4c21a23c3d41348059a
Troldmand43 <Tobiaseottsen@gmail.com>
2022-05-23 20:13:59 +0200

    Update of views in main fragment

--- app/src/main/java/com/example/weatherapp/view/SecondFragment.kt
+++ app/src/main/java/com/example/weatherapp/view/SecondFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherapp.R
